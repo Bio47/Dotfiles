c8fc2088a5675c83f39b4abf9d2ab6e3958e623f
Al <almilkar477@gmail.com>
2023-12-06 00:16:04 +0100

    Set coloscheme to gruvbox highcontrast

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,12 @@
 -- [[ Set a colorscheme with priority ]]
 return {
   -- 'morhetz/gruvbox',
-  'bluz71/vim-moonfly-colors',
+  -- 'bluz71/vim-moonfly-colors',
+  "iibe/gruvbox-high-contrast",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'moonfly'
+    vim.g.gruvbox_contrast_dark = "hard"
+    vim.cmd.colorscheme("gruvbox-high-contrast")
+    vim.api.nvim_set_option("background", "dark")
   end,
 }
